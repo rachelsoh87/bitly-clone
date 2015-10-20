af5f6934f51eeb70e22b5d9ff7f365f4505b6201
rachelsoh87 <ahsoh87@yahoo.com.my>
2015-10-20 13:44:51 +0000

    modified index.erb, create shorten method in model and created two fields for migration table

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,10 @@
 class Url < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
+	validates :shortened_url, uniqueness: true
+
+  def shorten
+    short_url = "http://cho.mped." + SecureRandom.base64(4)
+    self.shorten_url = short_url if self.shorten_url.nil?
+  end
+  
 end
--- db/migrate/20151020124129_create_urls.rb
+++ db/migrate/20151020124129_create_urls.rb
@@ -1,4 +1,8 @@
 class CreateUrls < ActiveRecord::Migration
 	def change
+		create_table :urls do |t|
+			t.string :longer_url
+			t.string :shortened_url
+		end			
 	end
 end
